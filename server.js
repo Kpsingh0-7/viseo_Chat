2168f73623fda1df610de6c10f8733f87728f8c6
Kpsingh0-7 <kpsingh954321@gmail.com>
2025-05-28 09:24:48 +0530

    16

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+  cors: { origin: "https://video-chat-82u9.onrender.com", methods: ["GET", "POST"] },
 });
 
 const PORT = 8080;
